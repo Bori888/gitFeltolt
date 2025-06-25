6b3b7683da3b00fd15efa21087fa8484d95b4407
Bori888 <ilyes.bori8@gmail.com>
2025-06-25 17:09:09 +0200

    feltolt

---
+++ src/feltoltgithub/FeltoltGitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package feltoltgithub;
+
+/**
+ *
+ * @author Bori
+ */
+public class FeltoltGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
